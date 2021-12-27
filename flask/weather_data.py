d10a958ab596912667e1a2428f300ee76fd26faa
MarilynWaldman <mdwaldman22@gmail.com>
2021-12-27 09:55:27 -0700

    usinging urllib3

--- flask/weather_data.py
+++ flask/weather_data.py
@@ -20,23 +20,8 @@ import shutil
 import requests
 import tarfile
 import datetime as dt
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-
-    return None
-
-def save_response_content(response,destination):
-    CHUNK_SIZE=32768
-    i = 1
-    with open(destination,"wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:
-#                st.write('Chunk:',i)
-                f.write(chunk)
-                i += 1
+import ssl
+import urllib3
 
 
 def get_weather_data(app):
@@ -44,36 +29,36 @@ def get_weather_data(app):
     # and we write output files to it
 
     #get latest wx warnings from NWS
-    url='https://tgftp.nws.noaa.gov/SL.us008001/DF.sha/DC.cap/DS.WWA/current_all.tar.gz'
-
-    session = requests.Session()
-
-    response = session.get(url,stream=True,verify=False)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'confirm':token}
-        response = session.get(url,params=params,stream=True,verify=False)
-
-    #have to set map path - used by template
-
     dest_path = os.path.join(os.getcwd(), 'downloads/')
      
     if os.path.exists(dest_path) and os.path.isdir(dest_path):
          shutil.rmtree(dest_path)
 
-    os.mkdir(dest_path)   
-    print("in weather data")  
-
+    os.mkdir(dest_path) 
     destination =  str(dest_path)+'current_all.tar.gz'
+    url='https://tgftp.nws.noaa.gov/SL.us008001/DF.sha/DC.cap/DS.WWA/current_all.tar.gz'
+    
+    http = urllib3.PoolManager()
+    resp = http.request(
+         "GET",
+          url,
+          preload_content=False)
+    print("response code")
+    print(resp.status)
 
-    save_response_content(response,destination)
+    
+    with open(destination,"wb") as f:
+        for chunk in resp.stream(32):
+                f.write(chunk)
+
+    resp.release_conn()
+
     wxdata = tarfile.open(name=destination)
+    
     wxdata.list(verbose=True)
 
     wxdata.extractall(path=str(dest_path)+'/current_all/')
 
-    print('extracted data')
 
     infile = str(dest_path) + '/current_all/current_all.shp'
 
